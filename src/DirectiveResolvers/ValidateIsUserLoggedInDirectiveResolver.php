9ffc237d374cd579336e35078bb1e4af442d3275
Leo <losoviz@gmail.com>
2020-02-20 19:40:33 +0800

    Use abstracted class

--- src/DirectiveResolvers/ValidateIsUserLoggedInDirectiveResolver.php
+++ src/DirectiveResolvers/ValidateIsUserLoggedInDirectiveResolver.php
@@ -1,54 +1,32 @@
 <?php
 namespace PoP\UserState\DirectiveResolvers;
 
-use PoP\ComponentModel\Feedback\Tokens;
 use PoP\Translation\Facades\TranslationAPIFacade;
-use PoP\ComponentModel\Facades\Engine\EngineFacade;
 use PoP\UserState\CheckpointSets\UserStateCheckpointSets;
 use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
-use PoP\ComponentModel\DirectiveResolvers\ValidateDirectiveResolver;
-use PoP\ComponentModel\GeneralUtils;
+use PoP\ComponentModel\DirectiveResolvers\AbstractValidateCheckpointDirectiveResolver;
 
-class ValidateIsUserLoggedInDirectiveResolver extends ValidateDirectiveResolver
+class ValidateIsUserLoggedInDirectiveResolver extends AbstractValidateCheckpointDirectiveResolver
 {
     const DIRECTIVE_NAME = 'validateIsUserLoggedIn';
     public static function getDirectiveName(): string {
         return self::DIRECTIVE_NAME;
     }
 
-    /**
-     * Check if the user is logged in
-     *
-     * @param TypeResolverInterface $typeResolver
-     * @param array $dataFields
-     * @param array $schemaErrors
-     * @param array $schemaWarnings
-     * @param array $schemaDeprecations
-     * @param array $variables
-     * @return void
-     */
-    protected function validateFields(TypeResolverInterface $typeResolver, array $dataFields, array &$schemaErrors, array &$schemaWarnings, array &$schemaDeprecations, array &$variables, array &$failedDataFields): void
+    protected function getValidationCheckpointSet(TypeResolverInterface $typeResolver): array
     {
-        $checkpointSet = UserStateCheckpointSets::LOGGEDIN_DATAFROMSERVER;
-        $engine = EngineFacade::getInstance();
-        $validation = $engine->validateCheckpoints($checkpointSet);
-        if (GeneralUtils::isError($validation)) {
-            $translationAPI = TranslationAPIFacade::getInstance();
-            // All fields failed
-            $failedDataFields = array_merge(
-                $failedDataFields,
-                $dataFields
-            );
-            $schemaErrors[] = [
-                Tokens::PATH => $dataFields,
-                Tokens::MESSAGE => sprintf(
-                    $translationAPI->__('You must be logged in to access fields: \'%s\'', 'user-state'),
-                    implode(
-                        $translationAPI->__('\', \''),
-                        $dataFields
-                    )
-                )
-            ];
-        }
+        return UserStateCheckpointSets::LOGGEDIN_DATAFROMSERVER;
+    }
+
+    protected function getValidationCheckpointErrorMessage(TypeResolverInterface $typeResolver, array $failedDataFields): string
+    {
+        $translationAPI = TranslationAPIFacade::getInstance();
+        return sprintf(
+            $translationAPI->__('You must be logged in to access fields \'%s\'', 'user-state'),
+            implode(
+                $translationAPI->__('\', \''),
+                $failedDataFields
+            )
+        );
     }
 }
