18cecc2359635966620df20ea656d0b65fe5897c
Leo <losoviz@gmail.com>
2020-02-21 17:33:18 +0800

    @validateIsUserLoggedIn must not be cached

--- src/Component.php
+++ src/Component.php
@@ -35,12 +35,16 @@ class Component extends AbstractComponent
         ContainerBuilderUtils::instantiateNamespaceServices(__NAMESPACE__.'\\Hooks');
         ContainerBuilderUtils::attachFieldResolversFromNamespace(__NAMESPACE__.'\\FieldResolvers');
         ContainerBuilderUtils::attachDirectiveResolversFromNamespace(__NAMESPACE__.'\\DirectiveResolvers');
+
         // Boot conditional on API package being installed
         if (class_exists('\PoP\API\Component')) {
             \PoP\UserState\Conditional\API\ComponentBoot::boot();
         }
         if (class_exists('\PoP\GraphQL\Component')) {
             \PoP\UserState\Conditional\GraphQL\ComponentBoot::boot();
+        }
+        if (class_exists('\PoP\CacheControl\Component')) {
+            \PoP\UserState\Conditional\CacheControl\ComponentBoot::boot();
         }
     }
 }
