49139e694a4ca79ec4ce18b0e8083f49fc042f84
Leo <losoviz@gmail.com>
2020-02-24 12:19:14 +0800

    Moved code under abstract class

---
+++ src/TypeResolverDecorators/AbstractValidateIsUserLoggedInTypeResolverDecorator.php
@@ -0,0 +1,39 @@
+<?php
+namespace PoP\UserState\TypeResolverDecorators;
+
+use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
+use PoP\ComponentModel\Facades\Schema\FieldQueryInterpreterFacade;
+use PoP\ComponentModel\TypeResolverDecorators\AbstractTypeResolverDecorator;
+use PoP\UserState\DirectiveResolvers\ValidateIsUserLoggedInDirectiveResolver;
+
+abstract class AbstractValidateIsUserLoggedInTypeResolverDecorator extends AbstractTypeResolverDecorator
+{
+    /**
+     * Verify that the user is logged in before checking the roles/capabilities
+     *
+     * @param TypeResolverInterface $typeResolver
+     * @return array
+     */
+    public function getMandatoryDirectivesForDirectives(TypeResolverInterface $typeResolver): array
+    {
+        $mandatoryDirectivesForDirectives = [];
+        $fieldQueryInterpreter = FieldQueryInterpreterFacade::getInstance();
+        // This is the required "validateIsUserLoggedIn" directive
+        $validateIsUserLoggedInDirective = $fieldQueryInterpreter->getDirective(
+            ValidateIsUserLoggedInDirectiveResolver::getDirectiveName()
+        );
+        // Add the mapping
+        foreach ($this->getDirectiveResolverClasses() as $needValidateIsUserLoggedInDirective) {
+            $mandatoryDirectivesForDirectives[$needValidateIsUserLoggedInDirective::getDirectiveName()] = [
+                $validateIsUserLoggedInDirective,
+            ];
+        }
+        return $mandatoryDirectivesForDirectives;
+    }
+    /**
+     * Provide the classes for all the directiveResolverClasses that need the "validateIsUserLoggedIn" directive
+     *
+     * @return array
+     */
+    abstract protected function getDirectiveResolverClasses(): array;
+}
