dd7f6d22ebaf2c32acf9f9181e3de343dd63ef38
Leo <losoviz@gmail.com>
2020-02-25 11:51:21 +0800

    Removed commented code

--- src/Conditional/GraphQL/ComponentBoot.php
+++ src/Conditional/GraphQL/ComponentBoot.php
@@ -30,11 +30,6 @@ class ComponentBoot
     protected static function attachDynamicDirectiveResolvers()
     {
         /**
-         * If `DISABLE_USER_STATE_FIELDS_IF_USER_NOT_LOGGED_IN` is true, then the schema (as obtained by querying the "__schema" field) is dynamic: Fields will be available or not depending on the user being logged in or not
-         * Then, the CacheControl for field "__schema" must be set to "no-cache"
-         */
-        // if (Environment::disableUserStateFieldsIfUserNotLoggedIn()) {
-        /**
          * If `USE_PRIVATE_SCHEMA_MODE` is true, then the schema (as obtained by querying the "__schema" field) is dynamic: Fields will be available or not depending on the user being logged in or not
          * Then, the CacheControl for field "__schema" must be set to "no-cache"
          */
--- src/Environment.php
+++ src/Environment.php
@@ -3,9 +3,6 @@ namespace PoP\UserState;
 
 class Environment
 {
-    // public static function disableUserStateFieldsIfUserNotLoggedIn(): bool
-    // {
-    //     return isset($_ENV['DISABLE_USER_STATE_FIELDS_IF_USER_NOT_LOGGED_IN']) ? strtolower($_ENV['DISABLE_USER_STATE_FIELDS_IF_USER_NOT_LOGGED_IN']) == "true" : false;
-    // }
+
 }
 
