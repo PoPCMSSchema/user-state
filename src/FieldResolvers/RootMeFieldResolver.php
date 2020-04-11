6b11ccce8e118553b23fc1c11f2cfad7899f1b8d
Leo <losoviz@gmail.com>
2020-04-11 17:26:55 +0800

    Moved code from trait to class

--- src/FieldResolvers/RootMeFieldResolver.php
+++ src/FieldResolvers/RootMeFieldResolver.php
@@ -3,13 +3,61 @@ namespace PoP\UserState\FieldResolvers;
 
 use PoP\API\TypeResolvers\RootTypeResolver;
 use PoP\UserState\FieldResolvers\AbstractUserStateFieldResolver;
+use PoP\ComponentModel\State\ApplicationState;
+use PoP\Users\TypeResolvers\UserTypeResolver;
+use PoP\ComponentModel\Schema\SchemaDefinition;
+use PoP\Translation\Facades\TranslationAPIFacade;
+use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
 
 class RootMeFieldResolver extends AbstractUserStateFieldResolver
 {
-    use MeFieldResolverTrait;
-
     public static function getClassesToAttachTo(): array
     {
         return array(RootTypeResolver::class);
+    }
+
+    public static function getFieldNamesToResolve(): array
+    {
+        return [
+            'me',
+        ];
+    }
+
+    public function getSchemaFieldType(TypeResolverInterface $typeResolver, string $fieldName): ?string
+    {
+        $types = [
+            'me' => SchemaDefinition::TYPE_ID,
+        ];
+        return $types[$fieldName] ?? parent::getSchemaFieldType($typeResolver, $fieldName);
+    }
+
+    public function getSchemaFieldDescription(TypeResolverInterface $typeResolver, string $fieldName): ?string
+    {
+        $translationAPI = TranslationAPIFacade::getInstance();
+        $descriptions = [
+            'me' => $translationAPI->__('The logged-in user', 'user-state'),
+        ];
+        return $descriptions[$fieldName] ?? parent::getSchemaFieldDescription($typeResolver, $fieldName);
+    }
+
+    public function resolveValue(TypeResolverInterface $typeResolver, $resultItem, string $fieldName, array $fieldArgs = [], ?array $variables = null, ?array $expressions = null, array $options = [])
+    {
+        switch ($fieldName) {
+            case 'me':
+                $vars = ApplicationState::getVars();
+                return $vars['global-userstate']['current-user-id'];
+        }
+
+        return parent::resolveValue($typeResolver, $resultItem, $fieldName, $fieldArgs, $variables, $expressions, $options);
+    }
+
+    public function resolveFieldTypeResolverClass(TypeResolverInterface $typeResolver, string $fieldName, array $fieldArgs = []): ?string
+    {
+        switch ($fieldName) {
+            case 'me':
+                return UserTypeResolver::class;
+        }
+
+        return parent::resolveFieldTypeResolverClass($typeResolver, $fieldName, $fieldArgs);
     }
 }
